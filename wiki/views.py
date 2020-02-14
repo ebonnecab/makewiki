c1f83be59730ac1dd0343ffaadeb0c5a4152ecab
ebonnecab <edotclaire@gmail.com>
2020-02-14 08:44:35 -0800

    finished v1

--- wiki/views.py
+++ wiki/views.py
@@ -2,44 +2,33 @@ from django.shortcuts import render
 from wiki.models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404,render
 
 
 class PageList(ListView):
     """
-    CHALLENGES:
-      1. On GET, display a homepage that shows all Pages in your wiki.
-      2. Replace this CHALLENGE text with a descriptive docstring for PageList.
-      3. Replace pass below with the code to render a template named `list.html`.
+    This view grabs all the pages out of the database
+    returns a list of each unique wiki page for the
+    user to access on the website through 'list.html'
     """
     model = Page
 
     def get(self, request):
         """ Returns a list of wiki pages. """
-        pass
-
+        pages = Page.objects.all()
+        context = {'pages': pages}
+        return render(request, 'list.html', context=context)
 
 class PageDetailView(DetailView):
     """
-    CHALLENGES:
-      1. On GET, render a template named `page.html`.
-      2. Replace this docstring with a description of what thos accomplishes.
-
-    STRETCH CHALLENGES:
-      1. Import the PageForm class from forms.py.
-          - This ModelForm enables editing of an existing Page object in the database.
-      2. On GET, render an edit form below the page details.
-      3. On POST, check if the data in the form is valid.
-        - If True, save the data, and redirect back to the DetailsView.
-        - If False, display all the errors in the template, above the form fields.
-      4. Instead of hard-coding the path to redirect to, use the `reverse` function to return the path.
-      5. After successfully editing a Page, use Django Messages to "flash" the user a success message
-           - Message Content: REPLACE_WITH_PAGE_TITLE has been successfully updated.
+    This view returns a page for a unique wiki using it's slug as an identifier
+    or a 404 message if the page does not exist
     """
     model = Page
-
+    
     def get(self, request, slug):
-        """ Returns a specific of wiki page by slug. """
-        pass
+      wiki = get_object_or_404(Page, slug=slug)
+      return render(request, 'page.html', {'wiki': wiki})
 
     def post(self, request, slug):
         pass
